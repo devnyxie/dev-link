2ad720088dc1cd3d087d75aaa62e80ef2e2fda6f
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-09-10 22:05:51 +0200

    Fix: XDG Open was causing an issue on Railway

--- devsync-react/vite.config.js
+++ devsync-react/vite.config.js
@@ -4,13 +4,14 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables from .env file
 const customPort = process.env.VITE_PORT || 5173; // Use CUSTOM_PORT if set, or default to 3000
-
+const isProduction =
+  process.env.VITE_MODE === 'production' || process.env.RAILWAY === 'true';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: Number(customPort),
-    open: true, // Set this to true to automatically open the browser
+    open: !isProduction, // Set this to true to automatically open the browser
   },
   define: {
     'process.env': dotenv.config().parsed,
