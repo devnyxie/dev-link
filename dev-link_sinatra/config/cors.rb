13de6de1ef972f3348b51459323786760b25d398
Tim Afanasiev <soupsoupchannel@gmail.com>
2023-10-15 00:34:25 +0200

    fix

--- dev-link_sinatra/config/cors.rb
+++ dev-link_sinatra/config/cors.rb
@@ -3,7 +3,7 @@ require 'rack/cors'
 configure do
   use Rack::Cors do
     allow do
-      origins 'https://dev-link.up.railway.app/'
+      origins 'https://dev-link.up.railway.app'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
     end
   end
