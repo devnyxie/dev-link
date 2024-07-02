039b2913989f654346a6942e5480c316e9fff2cb
Tim Afanasiev <84281349+devnyxie@users.noreply.github.com>
2024-07-02 23:59:03 +0200

    hotfix

--- apps/server/src/start.ts
+++ apps/server/src/start.ts
@@ -1,10 +1,24 @@
 import dotenv from "dotenv";
 import findWorkspaceRoot from "find-yarn-workspace-root";
 import path from "path";
+import fs from "fs";
 
 const startApp = () => {
   console.log("Server is starting...");
-  const workspaceRoot = findWorkspaceRoot();
+  
+  // Check if .env file exists in the current directory
+  const currentDir = process.cwd();
+  const envFilePath = path.resolve(currentDir, ".env");
+
+  if (fs.existsSync(envFilePath)) {
+    dotenv.config({ path: envFilePath });
+    require("./index");
+    return;
+  }
+
+  // If .env file is not found in the current directory, try to find it in the workspace root
+  const workspaceRoot = findWorkspaceRoot(currentDir);
+
   if (workspaceRoot) {
     const envPath = path.resolve(workspaceRoot, ".env");
     dotenv.config({ path: envPath });
